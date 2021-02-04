5b35f265dd79c8dbc8cd7367d2c89a471fb600e6
fatihhcan <ybsfatihcan@gmail.com>
2021-02-04 16:55:06 +0300

    last update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stateManagementExample/core/init/theme/theme.dart';
 import 'package:stateManagementExample/view/home/home.dart';
+import 'package:stateManagementExample/views/home/view/home_view.dart';
 import './core/extensions/contex_extension.dart';
 
 void main() => runApp(MyApp());
@@ -8,12 +9,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Material App',
-        theme: myTheme,
-        home: Scaffold(
-          appBar: AppBar(title: Text("data")),
-          body: Container(),
-        ));
+    return MaterialApp(title: 'Material App', theme: myTheme, home: HomeView());
   }
 }
